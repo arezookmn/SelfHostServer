4d8c46515aaff90f30395424c36e764b3d2afbf5
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-12-13 11:51:35 +0330

    self host server

---
+++ src/SelfHostServer.Host/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SelfHostServer.Host
+{
+    public class OrdersController : ApiController
+    {
+        private Order[] orders = new Order[]
+        {
+            new Order { Id = 1, Name = "Tomato", Category = "Groceries", Price = 1 },
+            new Order { Id = 2, Name = "YoYo", Category = "Toys", Price = 3.75M },
+            new Order { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+
+        };
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return orders;
+        }
+
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
